ab74adc9fdbce35a78a45b9df3f72366a356933b
Javier Vargas <ijavom@gmail.com>
2024-03-13 19:14:33 -0500

    Se explico template form y primera parte de formularios reactivos

--- src/app/tienda/productos/formulario/formulario.component.ts
+++ src/app/tienda/productos/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoriaService } from 'src/app/shared/services/categoria.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  formulario : FormGroup;
+  categorias : string[] = [];  
+  constructor(private formBuilder : FormBuilder, private categoriaService : CategoriaService) { 
+    this.formulario = this.formBuilder.group({
+      //Volvemos a las 8:20 - Ojo que el price como es numerico inicia en 0
+      title:['Un valor inicial',[Validators.required,Validators.minLength(6)]],
+      price: [0],
+      description: [''],
+      category: [''],
+      image: ['']
+
+    })
+  }
 
   ngOnInit(): void {
+    this.categoriaService.obtenerCategorias().subscribe({
+      next: (data)=>{
+        this.categorias = data;
+      },
+      error: (error)=>{
+        alert("Oh oh hubo un error")
+        console.log(error);
+      }
+    });
+  }
+
+  guardar(){
+    debugger;
   }
 
 }
--- src/app/tienda/productos/lista/lista.component.ts
+++ src/app/tienda/productos/lista/lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/shared/models/producto.model';
+import { CategoriaService } from 'src/app/shared/services/categoria.service';
 import { ProductosService } from 'src/app/shared/services/productos.service';
 
 @Component({
@@ -9,10 +10,23 @@ import { ProductosService } from 'src/app/shared/services/productos.service';
 })
 export class ListaComponent implements OnInit {
   productos: any = [];
+  filtroCantidad = 0;
+  categorias : string[] = [];
+  filtroCategoria = "";
 
-  constructor( private productosService : ProductosService) {}
+  constructor( private productosService : ProductosService, private categoriaService : CategoriaService) {}
 
   ngOnInit(): void {
+    this.categoriaService.obtenerCategorias().subscribe({
+      next: (data)=>{
+        this.categorias = data;
+      },
+      error: (error)=>{
+        alert("Oh oh hubo un error")
+        console.log(error);
+      }
+    })
+
     this.productosService.obtenerProductos().subscribe({
       next: (respuesta)=>{
         this.productos = respuesta;
@@ -25,5 +39,33 @@ export class ListaComponent implements OnInit {
         
       }
     });
+    //Antigua forma si descomentan se daran cuenta de que el visual studio nos advierte que esta "deprecated"
+    // this.productosService.obtenerProductos().subscribe(
+    //   (respuesta)=>{
+
+    // },
+    // (error)=>{
+
+    // },
+    // ()=>{
+
+    // });
+
+  }
+
+  filtrar(){
+    //funcionActivarCargador();
+    this.productosService.obtenerProductos(this.filtroCantidad,this.filtroCategoria).subscribe({
+      next: (respuesta)=>{
+        this.productos = respuesta;
+      },
+      error: (error)=>{
+        alert("Oh oh, algo paso!");
+        console.log(error);
+      },
+      complete: ()=>{
+        //funcionDesactivarCargador();
+      }
+    })
   }
 }
